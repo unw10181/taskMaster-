350d8fa5e1dd025918969d9cf67fa1a568257a97
unw10181 <uchennanwilliams@gmail.com>
2026-01-27 19:51:55 -0500

    server page

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`),
+  );
+});
