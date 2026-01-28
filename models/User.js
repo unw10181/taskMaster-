57c3b31f1b178deb0e2b5cbfb13a0121b5d480cd
unw10181 <uchennanwilliams@gmail.com>
2026-01-27 20:56:07 -0500

    commit error handling

--- models/User.js
+++ models/User.js
@@ -7,17 +7,10 @@ const userSchema = new Schema({
   password: { type: String, required: true },
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next();
-  }
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
-  try {
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods.isCorrectPassword = function (password) {
