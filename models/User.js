fcf7ed9cbbe002f4aed7e7965119a8cf69b2a753
unw10181 <uchennanwilliams@gmail.com>
2026-01-27 20:11:08 -0500

    user Schema

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.isCorrectPassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = model("User", userSchema);
