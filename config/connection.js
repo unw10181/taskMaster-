ea03d3d33621ceb0b0cdcb096b2651da3dd2c097
unw10181 <uchennanwilliams@gmail.com>
2026-01-27 20:35:41 -0500

    securing connection to mgdb

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,15 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("MongoDB connection error:", err.message);
+});
 
 module.exports = mongoose.connection;
