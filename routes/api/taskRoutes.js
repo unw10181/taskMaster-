1271141ee90bd4badb03c99e4a581168f74045c3
unw10181 <uchennanwilliams@gmail.com>
2026-01-27 20:14:36 -0500

    createtask route

---
+++ routes/api/taskRoutes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Task, Project } = require("../../models");
+const { authMiddleware } = require("../../utils/auth");
+
+router.use(authMiddleware);
+
+// Create task
+router.post("/projects/:projectId/tasks", async (req, res) => {
+  const project = await Project.findById(req.params.projectId);
+  if (!project || project.user.toString() !== req.user._id) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  const task = await Task.create({
+    ...req.body,
+    project: project._id,
+  });
+
+  res.json(task);
+});
+
+
+module.exports = router;
